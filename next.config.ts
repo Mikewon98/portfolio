b36035c49d6e95b6a25d225e02ff8dcd807f1f81
mikewon98a <mike@aquilaict.com>
2025-12-17 20:57:00 +0300

    fix: add cache control headers to prevent server action mismatch and updated CI/CD to use immutable tags

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,28 @@ const nextConfig: NextConfig = {
     ],
   },
   output: "standalone",
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=0, must-revalidate",
+          },
+        ],
+      },
+      {
+        source: "/_next/static/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
